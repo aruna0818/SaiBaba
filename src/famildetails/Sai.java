28f7009d5a93441e20b535d2e75ad3fa1e763a9e
aruna0818 <arunachalam3452@gmail.com>
2020-06-12 19:42:13 +0530

    aruna

---
+++ src/famildetails/Sai.java
@@ -0,0 +1,22 @@
+package famildetails;
+
+public class Sai {
+	
+	public void name() {
+System.out.println("arunachalam");
+	}
+	public void dob() {
+		System.out.println("18.03.1997");
+	}
+	
+	public void mob() {
+System.out.println("9789814217");
+	}
+	public static void main(String[] args) {
+		Sai a=new Sai();
+		a.name();
+		a.dob();
+		a.mob();
+	}
+}
+
